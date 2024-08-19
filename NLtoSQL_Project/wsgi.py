95e2f75b6096a629cd58e13d8c5436875b39ba21
Ayanleaideed <Ayanleaideed@outlook.com>
2024-08-18 23:45:48 -0500

    Deploying on Vercel v4

--- NLtoSQL_Project/wsgi.py
+++ NLtoSQL_Project/wsgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NLtoSQL_Project.settings")
 
-application = get_wsgi_application()
-app = application
+try:
+    application = get_wsgi_application()
+    app = application
+except Exception as e:
+    print(f"An error occurred: {e}", file=sys.stderr)
+    raise e
