e547683da061308b4a8f22fe3f586c3045d5baf2
Ayanle <116397906+Ayanleaideed@users.noreply.github.com>
2024-08-23 02:14:55 -0500

    Update settings.py

--- NLtoSQL_Project/settings.py
+++ NLtoSQL_Project/settings.py
@@ -22,7 +22,7 @@ DEBUG =  os.environ.get("DEBUG", "False").lower() == "true"
 
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split("")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', "https://natural-language-to-sql.onrender.com").split(" ")
 
 
 
