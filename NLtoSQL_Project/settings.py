2d60e02c00daec2deb0cc23f574642374be28951
Ayanleaideed <Ayanleaideed@outlook.com>
2024-08-23 21:31:23 -0500

    Adjusting the loader for query page.

--- NLtoSQL_Project/settings.py
+++ NLtoSQL_Project/settings.py
@@ -16,11 +16,11 @@ environ.Env.read_env()
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "your-default-secret-key-here")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
